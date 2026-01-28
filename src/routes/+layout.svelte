499a95e936abb0b50e133adb355c72cd536c9843
KTibow <KTibow@users.noreply.github.com>
2026-01-27 19:44:33 -0800

    favicon as asset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import favicon from "../favicon.svg";
   import "../app.css";
 
   let { children } = $props();
 </script>
 
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
